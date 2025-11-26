1d6df3e09dcbba06b22b31ccdeb562d74b1c3502
kuangdang123 <liangjiaqicy@163.com>
2025-11-26 10:51:52 +0800

    添加config文件

---
+++ config.py
@@ -0,0 +1,9 @@
+train_config = {
+    "model_path" : "yolov8s-oiv7.pt",
+    "trian_dataset_path" : "./export_oi/dataset.yaml"
+}
+
+test_config = {
+    "model_path" : "garbage_detection_precise/stage4_full_finetune/weights/best.pt",
+    "imgs_path" : "export_oi/images/test",
+}
